77881947325f954345fe62f28cce4f4b0188ea72
Adéla Přibylová <adelpribyl@gmail.com>
2024-06-01 16:37:09 +0200

    Second part questino 16

--- src/components/Joke/index.jsx
+++ src/components/Joke/index.jsx
@@ -1,1 +1,45 @@
-export const Joke = ({ userAvatar, userName, text, likes, dislikes }) => {};
+import { useState } from 'react';
+import './style.css';
+
+export const Joke = ({ userAvatar, userName, text, likes, dislikes }) => {
+  const [onLikes, setOnLikes] = useState(0);
+  const [onDislikes, setOnDislikes] = useState(0);
+
+  const handleClickLike = () => {
+    setOnLikes(onLikes + 1);
+  };
+
+  const handleClickDislike = () => {
+    setOnDislikes(onDislikes + 1);
+  };
+
+  return (
+    <div class="joke">
+      <div class="joke__body">
+        <div class="joke__user">
+          <img class="user-avatar" src={userAvatar} />
+          <p class="user-name">{userName}</p>
+        </div>
+        <p class="joke__text">{text}</p>
+      </div>
+      <div class="joke__likes">
+        <button
+          id="btn-up"
+          class="btn-like btn-like--up"
+          onClick={handleClickLike}
+        ></button>
+        <span id="likes-up" class="likes-count likes-count--up">
+          {onLikes}
+        </span>
+        <button
+          id="btn-down"
+          class="btn-like btn-like--down"
+          onClick={handleClickDislike}
+        ></button>
+        <span id="likes-down" class="likes-count likes-count--down">
+          {onDislikes}
+        </span>
+      </div>
+    </div>
+  );
+};
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,55 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './style.css';
+import { Joke } from '../../components/Joke';
 
 export const HomePage = () => {
-  const [likes, setLikes] = useState(0);
-  const [dislikes, setDislikes] = useState(0);
+  const [jokes, setJokes] = useState(null);
 
-  const handleClickLike = () => {
-    setLikes(likes + 1);
-  };
-
-  const handleClickDislike = () => {
-    setDislikes(dislikes + 1);
-  };
+  useEffect(() => {
+    const readJokes = async () => {
+      const response = await fetch('http://localhost:4000/api/jokes');
+      const json = await response.json();
+      setJokes(json.data);
+    };
+    readJokes();
+  }, []);
 
   return (
     <div className="container">
-      <div class="joke">
-        <div class="joke__body">
-          <div class="joke__user">
-            <img
-              class="user-avatar"
-              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-            />
-            <p class="user-name">Neroxx</p>
-          </div>
-
-          <p class="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div class="joke__likes">
-          <button
-            id="btn-up"
-            class="btn-like btn-like--up"
-            onClick={handleClickLike}
-          ></button>
-          <span id="likes-up" class="likes-count likes-count--up">
-            {likes}
-          </span>
-          <button
-            id="btn-down"
-            class="btn-like btn-like--down"
-            onClick={handleClickDislike}
-          ></button>
-          <span id="likes-down" class="likes-count likes-count--down">
-            {dislikes}
-          </span>
-        </div>
-      </div>
+      {jokes !== null &&
+        jokes.map((joke) => (
+          <Joke
+            key={joke.id}
+            userAvatar={joke.avatar}
+            userName={joke.name}
+            text={joke.text}
+            likes={joke.likes}
+            dislikes={joke.dislikes}
+          />
+        ))}
     </div>
   );
 };
